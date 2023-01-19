40dc876ecb50c7bf21c5116b2063161b66c32d8a
lavish0000 <lavick100@gmail.com>
2023-01-19 15:14:08 +0530

    Chore: add server online socket event

--- src/modules/Socket/index.ts
+++ src/modules/Socket/index.ts
@@ -3,6 +3,9 @@ import Connection from './connection';
 
 function socket(io: Server) {
   global.io = io;
+  setInterval(() => {
+    io.emit('server_online', global.machineKeyPair.address);
+  }, 5000);
   io.on('connection', (socket) => {
     console.log('a user connected');
     
