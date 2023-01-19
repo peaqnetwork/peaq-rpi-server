7d9f650c0651a02ebf75f1674d5f0a67f999be80
lavish0000 <lavick100@gmail.com>
2023-01-19 15:19:49 +0530

    chore: add startup, update scripts

---
+++ startup.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Function to handle errors
+error_handler() {
+  echo "Error: An error occurred at line $1"
+  exit 1
+}
+
+# Trap errors
+trap 'error_handler $LINENO' ERR
+
+# Check if Node.js is installed
+if ! command -v node &> /dev/null
+then
+    # Log message
+    echo "Installing Node.js..."
+
+    # Check if nvm is installed
+    if ! command -v nvm &> /dev/null
+    then
+        # Log message
+        echo "Installing nvm..."
+
+        # Install nvm
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    fi
+
+    # Load nvm
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    # Install the latest stable version of Node.js using nvm
+    nvm install stable
+fi
+
+# Update package index files
+sudo apt-get update
+
+# Check if Nginx is installed
+if ! command -v nginx &> /dev/null
+then
+    # Log message
+    echo "Installing Nginx..."
+
+    # Install Nginx
+    sudo apt-get install -y nginx
+fi
+
+# Create the local script file
+# touch local_script.sh
+
+# Make the script executable
+# chmod +x local_script.sh
+
+# echo "@reboot ./local_script.sh" | crontab -
+
+# Log message
+echo "Cloning Node.js server repository..."
+
+# Clone the Node.js server repository
+git clone https://github.com/peaqnetwork/peaq-rpi-server-build.git
+
+# Log message
+echo "Installing PM2..."
+
+# Install PM2
+npm install pm2@latest -g
+
+# Log message
+echo "Starting server using PM2..."
+
+# Start the server using PM2
+
+pm2 start peaq-rpi-server-build/src/server.js
+
+# Start the update script using PM2 which will be restarted every 20 seconds (update the time interval to 1 min)
+pm2 start bash --exp-backoff-restart-delay=30000  --name "startup-script" -- -c "curl -H 'Cache-Control: no-cache, no-store' -s https://raw.githubusercontent.com/peaqnetwork/peaq-rpi-server/update.sh | bash"
+
+# Save the PM2 process list	
+pm2 save
+
+# Extract the Startup Script command from the output of 'pm2 startup'
+startup_script=$(pm2 startup | awk '/sudo/ {$1=""; print "sudo " $0}')
+
+# Execute the Startup Script command
+eval "$startup_script"
+
+# Save the PM2 process list	
+pm2 save
+
+# Log success message	
+echo "Script completed successfully!"
---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# PATH=/home/pi/.nvm/versions/node/v19.4.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+# HOME=/home/pi
+# pm2_path=$(which pm2)
+# export PATH=$PATH:$pm2_path
+
+cd ~
+version=1.0
+stored_version=$(cat .update/version 2>/dev/null)
+
+run_script() {
+  mkdir test_auto_updates
+}
+
+if [ ! -d ".update" ]; then
+  echo "Creating .update directory" | tee -a .update/logs.txt
+  mkdir .update || { echo "Error: Failed to create .update directory" | tee -a .update/logs.txt; exit 1; }
+fi
+
+if [ -z "$stored_version" ]; then
+  echo "Creating version file" | tee -a .update/logs.txt
+  touch .update/version || { echo "Error: Failed to create version file" | tee -a ./.update/logs.txt; exit 1; }
+  echo "Setting permissions on version file" | tee -a .update/logs.txt
+  chmod 766 .update/version || { echo "Error: Failed to set permissions on version file" | tee -a ./.update/logs.txt; exit 1; }
+  echo "Writing version to version file" | tee -a .update/logs.txt
+  echo "$version" > ./.update/version || { echo "Error: Failed to write version to version file" | tee -a .update/logs.txt; exit 1; }
+  echo "Running script" | tee -a .update/logs.txt
+  run_script || { echo "Error: Failed to run script" | tee -a .update/logs.txt; exit 1; }
+else
+  if [ "$version" \> "$stored_version" ]; then
+    echo "Updating version in version file" | tee -a .update/logs.txt
+    echo "$version" > .update/version || { echo "Error: Failed to update version in version file" | tee -a ./.update/logs.txt; exit 1; }
+    echo "Running script" | tee -a .update/logs.txt
+    run_script || { echo "Error: Failed to run script" | tee -a .update/logs.txt; exit 1; }
+  else
+    echo "Skipping script because stored version ($stored_version) is greater than or equal to current version ($version)" | tee -a .update/logs.txt
+  fi
+fi
